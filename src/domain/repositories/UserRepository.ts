83b54a86ea3cd6ef8719e19121331de503bd8caf
slimwolff <ptudoandtodos@gmail.com>
2025-10-24 17:25:53 -0300

    delete clean arch folder structure

--- src/domain/repositories/UserRepository.ts
+++
@@ -1,6 +0,0 @@
-export interface IUserRepository {
-    create(data: CreateUserDTO): Promise<User>
-    findById(id: string): Promise<User | null>
-    update(id: string, data: UpdateUserDTO): Promise<User | null>
-    delete(id: string): Promise<void>
-}
