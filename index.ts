080a3a3ab83306b1fffbbe0f1f833060b19db66f
slimwolff <ptudoandtodos@gmail.com>
2025-10-24 17:24:37 -0300

    feat: add register route

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
+import userRoutes from "./src/routes/user.routes.js"
 
 const app = fastify();
+
+app.register(userRoutes, {prefix: "/v1"})
 
 app.listen({port:3000}, (err) => {
     if(err) {
