8c74186ec8149a1f1992a49c8686a8c08e72c636
slimwolff <ptudoandtodos@gmail.com>
2025-10-28 17:10:20 -0300

    define options to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,38 @@
-import userController from "../controllers/user.controller.ts"
+import type { FastifyInstance, FastifySchema } from "fastify"
+import {getUser, createUser} from "../controllers/user.controller.ts"
 
 
-export default function (fastify, opts, done) {
-    fastify.get("/user", userController)
+export default function (fastify: FastifyInstance , _opts: FastifySchema, done) {
+    fastify.route({
+        method: 'GET',
+        url: '/user',
+        schema: {
+            response: {
+                200: {
+                    type: 'object',
+                    properties: {
+                        res: { type: 'string'}
+                    }
+                }
+            }
+        },
+        handler: getUser
+    })
+    fastify.post("/user", {
+        schema: {
+            body: {
+                
+                name: 'string',
+            },
+            response: {
+                200: {
+                    type: 'object',
+                    properties: {
+                        "name": { type: 'string' }
+                    }
+                }
+            }
+        }
+    },createUser)
     done()
 }
