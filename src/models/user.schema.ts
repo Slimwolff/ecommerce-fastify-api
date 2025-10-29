337d70dd9bab5abf649c40a3ae077fefbc7346c4
slimwolff <ptudoandtodos@gmail.com>
2025-10-29 17:28:52 -0300

    add fields to user schema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,6 +1,22 @@
 import z from "zod";
 
+const genderEnum = z.enum(["M","F","N","O"])
+const passwordSchema = z.string().min(8).refine(
+    (v) =>
+        /[A-Z]/.test(v) &&
+        /[0-9]/.test(v) &&
+        /[^A-Za-z0-9]/.test(v),
+        "Password must contain at least one uppercase letter, one number, and one symbol"
+)
+
 const createUserSchema = z.object({
-    first_name: z.string(),
-    last_name: z.string()
+    first_name: z.string().max(255),
+    last_name: z.string().max(255),
+    birthdate: z.date(),
+    gender: genderEnum,
+    address: z.object(),
+    email: z.email(),
+    password: passwordSchema
 })
+
+type CreateUserInput = z.infer<typeof createUserSchema>
